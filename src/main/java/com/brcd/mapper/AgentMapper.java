8e41809495d976fc22826bc97c024287eeb8c7bc
hmz <286676167@qq.com>
2017-09-07 10:06:39 +0800

    基本功能提交

---
+++ src/main/java/com/brcd/mapper/AgentMapper.java
@@ -0,0 +1,34 @@
+package com.brcd.mapper;
+
+import com.brcd.bean.TbAgent;
+import com.github.pagehelper.Page;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * 代理商管理持久层
+ * Created by 韩明泽 on 2017/9/5.
+ */
+@Mapper
+@Repository
+public interface AgentMapper {
+  /**
+   * 测试
+   * @return
+   */
+  List<TbAgent> findAll();
+
+  /**
+   * 查询所有代理上信息
+   * @return
+   */
+  Page<TbAgent> getAgent(TbAgent agent);
+  /**
+   * 根据id查询代理商详细信息
+   * @param id
+   * @return
+   */
+  TbAgent findAgentById(Long id);
+}
