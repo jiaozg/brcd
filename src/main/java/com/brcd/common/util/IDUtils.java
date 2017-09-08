58f4c7525a44f59900c723f5e608fb432345f80f
zhumengguang <zmengguang@126.com>
2017-09-08 13:41:07 +0800

    添加交易结算

--- src/main/java/com/brcd/common/util/IDUtils.java
+++ src/main/java/com/brcd/common/util/IDUtils.java
@@ -2,11 +2,6 @@ package com.brcd.common.util;
 
 import java.util.Random;
 
-/**
-
- * id生成工具类
- */
-
 
 public class IDUtils {
 
@@ -22,16 +17,15 @@ public class IDUtils {
 		int end3 = random.nextInt(999);
 		//如果不足三位前面补0
 		String str = millis + String.format("%03d", end3);
-		
+
 		return str;
 	}
-	
+
 	
 	/**
-	 * id生成
+	 * 商户id生成
 	 */
-	public static String genItemId() {
-
+	public static Long genItemId() {
 		//取当前时间的长整形值包含毫秒
 		long millis = System.currentTimeMillis();
 		//long millis = System.nanoTime();
@@ -40,10 +34,8 @@ public class IDUtils {
 		int end2 = random.nextInt(99);
 		//如果不足两位前面补0
 		String str = millis + String.format("%02d", end2);
-
 		long id = new Long(str);
 		return id;
-
 	}
 	
 	public static void main(String[] args) {
