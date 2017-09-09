2f3617c15b3274beb203ecfcb448812cefbd2743
zhaohan <694676765@qq.com>
2017-09-09 12:55:12 +0800

    qq

--- src/main/java/com/brcd/service/impl/TbBusinessUserServiceImpl.java
+++ src/main/java/com/brcd/service/impl/TbBusinessUserServiceImpl.java
@@ -54,7 +54,7 @@ public class TbBusinessUserServiceImpl implements TbBusinessUserService {
     }
 
     /**
-     * 添加商户开通线下支付信息
+     * 添加商户开通线下支付信息更改
      */
     private void insertBusiness(TbBusiness business) {
 
