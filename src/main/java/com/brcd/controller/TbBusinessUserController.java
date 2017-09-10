d23e5ce6546dbaae1f1b1ebb4267c12b70c62983
admin <2568796979@qq.com>
2017-09-10 20:04:38 +0800

    bxj3

--- src/main/java/com/brcd/controller/TbBusinessUserController.java
+++ src/main/java/com/brcd/controller/TbBusinessUserController.java
@@ -22,7 +22,6 @@ import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
